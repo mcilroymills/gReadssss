5023620ffdb12c6f95ea55d8a12996fcbd8f25e8
millsmcilroy <millsmcilroy@gmail.com>
2016-03-28 14:34:32 -0600

    added book routes

---
+++ src/server/routes/books.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+var queries = require('../../../db/queries');
+
+router.get('/', function(req, res, next) {
+  res.render('books', { title: 'Express' });
+});
+
+router.get('/:id', function(req, res, next) {
+  res.render('book_show', { title: 'Express' });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('book_new_edit', { title: 'Express' });
+});
+
+router.get('/:id/edit', function(req, res, next) {
+  res.render('book_new_edit', { title: 'Express' });
+});
+
+router.post('/new', function(req, res, next) {
+  queries.addBook(req.body)
+  .then(function() {
+    res.redirect('/');
+  })
+  .catch(function(err) {
+    console.log('Error:', err);
+    return err;
+  });
+});
+
+router.post('/:id/edit', function(req, res, next) {
+  queries.editBook(req.body, req.params.id)
+  .then(function() {
+    res.redirect('/' + req.params.id);
+  })
+  .catch(function(err) {
+    console.log('Error:', err);
+    return err;
+  });
+});
+
+router.post('/:id/delete', function(req, res, next) {
+  queries.deleteBook(req.params.id)
+  .then(function() {
+    res.redirect('/');
+  })
+  .catch(function(err) {
+    console.log('Error:', err);
+    return err;
+  });
+});
+
+module.exports = router;
