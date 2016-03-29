7b6d0ca7deda653c6d7581f9b350a90e2260c595
millsmcilroy <millsmcilroy@gmail.com>
2016-03-28 21:05:19 -0600

    updated migrations to cascade on delete

--- db/migrations/20160328111755_catalog.js
+++ db/migrations/20160328111755_catalog.js
@@ -1,8 +1,8 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('catalog', function(table){
-    table.integer('book_id').references('id').inTable('books');
-    table.integer('author_id').references('id').inTable('authors');
+    table.integer('book_id').references('id').inTable('books').onDelete('CASCADE');
+    table.integer('author_id').references('id').inTable('authors').onDelete('CASCADE');
   });
 
 };
--- db/migrations/20160328111808_books_genres.js
+++ db/migrations/20160328111808_books_genres.js
@@ -1,8 +1,8 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('books_genres', function(table){
-    table.integer('book_id').references('id').inTable('books');
-    table.integer('genre_id').references('id').inTable('genres');
+    table.integer('book_id').references('id').inTable('books').onDelete('CASCADE');
+    table.integer('genre_id').references('id').inTable('genres').onDelete('CASCADE');
   });
 
 };
